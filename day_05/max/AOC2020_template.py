da84bd2496aa017c4eaffcaa32055514806b28ca
redTribune <74654478+redTribune@users.noreply.github.com>
2020-11-30 13:53:48 +0100

    Add files via upload

---
+++ day_05/max/AOC2020_template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "redTribune"
+__version__ = "0.1"
+__license__ = "MIT"
+
+# This code was created for the 'Advent Of Code' advent challenge 2020. Please feel free to use or modify it.
+# I can NOT in any way guarantee that it is doing what it was supposed to do. Or even running... XD
+
+import numpy as np
+import csv
+from copy import deepcopy
+
+if __name__=="__main__":
+
+    """
+    body
+    """
+    
+    result = "fancyStuff"
+    print("Answer Code: %s" % (result))
