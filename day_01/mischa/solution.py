9786e7d166136097b3fcdd8f7e836a8d47f6fcac
voidlessVoid <58170899+voidlessVoid@users.noreply.github.com>
2020-12-01 07:15:25 +0100

    add day 1

--- day_01/mischa/solution.py
+++ day_01/mischa/solution.py
@@ -15,15 +15,32 @@ os.chdir(CURRENT_DIRECTORY)
 
 def read_input_lines():
     with open('input.txt', 'r') as fh:
-        return [x.strip() for x in fh.readlines()]
+        return [int(x.strip()) for x in fh.readlines()]
 
 def read_input_text():
     with open('input.txt', 'r') as fh:
         return fh.read().strip()
+data=read_input_lines()
+df = pd.DataFrame(data, columns=['nums'])
 
 
-def part_a():
-    pass
 
-def part_b():
-    pass
+def part_a(inp):
+    for i in inp['nums']:
+        temp = inp.add(i)
+        if 2020 in temp['nums'].values:
+            return print('answerA', (2020 - i) * i)
+
+
+def part_b(inp):
+    for i in inp['nums']:
+        temp = inp.add(i)
+        temp1 = temp[temp.nums <2020]
+        for x in inp['nums']:
+            temp2 = temp1.add(x)
+            if 2020 in temp2['nums'].values:
+                return print('answerB', (i*x*(2020-i-x)))
+part_a(df)
+part_b(df)
+#answerA 437931
+#answerB 157667328
