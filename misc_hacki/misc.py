b8537423d7c8503fdc197588fc6799183228b12a
hacki <sebastian.hackler@gmx.de>
2020-12-02 13:28:02 +0100

    my way to solve day 2

--- misc_hacki/misc.py
+++ misc_hacki/misc.py
@@ -5,5 +5,9 @@ def path_to_dir(filename):
 def load_input_to_list(path):
     with open(path) as file:
         content = file.readlines()
-    content = [int(x.strip()) for x in content]
+        try:
+            content = [int(x.strip()) for x in content]
+        except:
+            content = [str(x.strip()) for x in content]
+
     return content
