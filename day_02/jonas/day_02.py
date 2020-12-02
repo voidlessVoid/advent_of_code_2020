25fd4b2f24b268ea6cacb7c76baa49ac92d2f70d
js-py20 <75324584+js-py20@users.noreply.github.com>
2020-12-02 17:14:37 +0100

    Update day_02.py

--- day_02/jonas/day_02.py
+++ day_02/jonas/day_02.py
@@ -28,3 +28,28 @@ def check(data):
     
 
 check(data_list)
+
+
+def check2(data):
+    for i in range(len(data)):
+        results = []
+        for x in (data):
+            lst = x.split()
+            letter = lst[1].replace(":","")
+            def split(word): 
+                return [char for char in word]
+            password = split(lst[2])
+            boundaries = lst[0].split("-")
+            pos_1 = int(boundaries[0])-1
+            pos_2 = int(boundaries[1])-1
+            if letter == password[pos_1] and letter != password[pos_2]:
+                results.append(True)
+            if letter == password[pos_2] and letter != password[pos_1]:
+                results.append(True)
+            else:
+                results.append(False)            
+    print(results.count(True))
+    
+    
+
+check2(data_list)
