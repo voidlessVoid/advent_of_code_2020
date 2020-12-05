fb5f827a7ddfc9fae5a0caec9d6af05bef7a2044
Gerrit Begher <gbegher@gmail.com>
2020-12-05 18:02:46 +0100

    [ADD] gerrit: days 2, 3, 4

---
+++ day_04/gerrit/util/__init__.py
@@ -0,0 +1,33 @@
+def pipe(x):
+    def piper(*functions):
+        result = x
+        for fn in functions:
+            result = fn(x)
+
+        return result
+
+    return piper
+
+def filter(generator, predicate):
+    for item in generator:
+        if predicate(item):
+            yield item
+
+
+def split_by(predicate):
+    def splitter(items):
+        collected = []
+
+        for item in items:
+            if predicate(item):
+                if len(collected) is not 0:
+                    yield collected
+                    collected = []
+            else:
+                collected = [*collected, item]
+
+        if len(collected) is not 0:
+            yield collected
+            collected = []
+
+    return splitter
