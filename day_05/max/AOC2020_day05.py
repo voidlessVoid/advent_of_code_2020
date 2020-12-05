09ef8c03bf8a432c63780ade2c5aa9f529623989
redTribune <74654478+redTribune@users.noreply.github.com>
2020-12-05 14:59:08 +0100

    Add files via upload

--- day_05/max/AOC2020_day05.py
+++ day_05/max/AOC2020_day05.py
@@ -10,52 +10,39 @@ __license__ = "MIT"
 
 import numpy as np
 
+
 def eval(code, rows, col, limit=None):
     
     def FRONT(interval):
-        if np.sum(interval)%2 != 0:
-            return [interval[0], interval[1] + 1 - (interval[1] - interval[0] + 1)/2 - 1]
-        
-        else:
-            return [interval[0], interval[1] - (interval[1] - interval[0])/2]
+        if np.sum(interval)%2 != 0: return [interval[0], interval[1] + 1 - (interval[1] - interval[0] + 1)/2 - 1]
+        else: return [interval[0], interval[1] - (interval[1] - interval[0])/2]
     
     def BACK(interval):
-        if np.sum(interval)%2 != 0:
-            return [interval[1] + 1 - (interval[1] - interval[0] + 1)/2, interval[1]]
-        
-        else:
-            return [interval[1] - (interval[1] - interval[0])/2, interval[1]]
+        if np.sum(interval)%2 != 0: return [interval[1] + 1 - (interval[1] - interval[0] + 1)/2, interval[1]]
+        else: return [interval[1] - (interval[1] - interval[0])/2, interval[1]]
     
     def LEFT(interval):
-        if np.sum(interval)%2 != 0:
-            return [interval[0], interval[1] + 1 - (interval[1] - interval[0] + 1)/2 - 1]
-        
-        else:
-            return [interval[0], interval[1] - (interval[1] - interval[0])/2]
+        if np.sum(interval)%2 != 0: return [interval[0], interval[1] + 1 - (interval[1] - interval[0] + 1)/2 - 1]
+        else: return [interval[0], interval[1] - (interval[1] - interval[0])/2]
     
     def RIGHT(interval):
-        if np.sum(interval)%2 != 0:
-            return [interval[1] + 1 - (interval[1] - interval[0] + 1)/2, interval[1]]
-        
-        else:
-            return [interval[1] - (interval[1] - interval[0])/2, interval[1]]
+        if np.sum(interval)%2 != 0: return [interval[1] + 1 - (interval[1] - interval[0] + 1)/2, interval[1]]
+        else: return [interval[1] - (interval[1] - interval[0])/2, interval[1]]
     
     lines = [0, rows]
     seats = [0, col]
     for idx in range(7):
         if code[idx].upper() == "F": lines = FRONT(lines)
         elif code[idx].upper() == "B": lines = BACK(lines)
-        else:
-            raise ValueError
+        else: raise ValueError
         if limit != None:
-           if col + lines[1] * 8 < limit: return None
+            if col + lines[1] * 8 < limit: return None
         
     line = lines[1]
     for idx in range(3):
         if code[idx + 7].upper() == "R": seats = RIGHT(seats)
         elif code[idx + 7].upper() == "L": seats = LEFT(seats)
-        else:
-            raise ValueError
+        else: raise ValueError
         if limit != None:
             if line * 8 + seats[1] < limit: return None
         
@@ -64,21 +51,17 @@ def eval(code, rows, col, limit=None):
              
 
 if __name__=="__main__":
-    
     plane = list()
     with open("input.txt") as getInp:
-        for line in getInp:
-            plane.append(line.strip())
+        for line in getInp: plane.append(line.strip())
     
     limit = 0               
     for pssngr in plane:
         val = eval(pssngr, 127, 7, limit=limit)
-        if val != None:
-            limit = val[0] * 8 + val[1]
+        if val != None: limit = val[0] * 8 + val[1]
     
     planeMap = np.array([])
-    for idx in range(128):
-        planeMap = np.concatenate((planeMap, np.arange(8)), axis=0)
+    for idx in range(128): planeMap = np.concatenate((planeMap, np.arange(8)), axis=0)
      
     planeMap = planeMap.reshape(128,8) 
         
@@ -87,12 +70,10 @@ if __name__=="__main__":
         if planeMap[val[0]][val[1]] == -1: print(val, pssngr)
         planeMap[val[0]][val[1]] = -1
 
-    
     mySeat = np.where(planeMap != -1)  
     mySeatLine = mySeat[0]
     seatID = list()
-    for idx, line in enumerate(mySeatLine):
-        seatID.append(mySeat[0][idx] * 8 + mySeat[1][idx])
+    for idx, line in enumerate(mySeatLine): seatID.append(mySeat[0][idx] * 8 + mySeat[1][idx])
         
     for idx, element in enumerate(seatID):
         if idx == 0 or idx == len(seatID) - 1: continue
@@ -101,5 +82,4 @@ if __name__=="__main__":
             break
     
     result = "Answer 1: " + str(limit) + "\tAnswer 2: " + str(mySeatID)
-    
     print("Answer Code: %s" % (result))
