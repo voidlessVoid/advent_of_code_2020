20cc4cfdaf0538d3783fabd882115e4e38f20000
js-py20 <75324584+js-py20@users.noreply.github.com>
2020-12-06 13:02:08 +0100

    Create day_06.py

---
+++ day_06/jonas/day_06.py
@@ -0,0 +1,55 @@
+import string
+
+
+def input():
+    with open('input_06.txt', 'r') as file:
+        return file.read().strip()
+
+answers = input().split("\n\n")
+abc = list(string.ascii_lowercase)
+
+def part_a():
+	result_list = []
+	for answer in answers:
+		answer_ordered = answer.replace("\n","")
+		result = 0
+		for char in abc:
+			if char in answer_ordered:
+				result +=1
+		result_list.append(result)
+	print(sum(result_list))
+
+part_a()
+
+answers_astyle = []
+for answer in answers:
+	answer_ordered = answer.replace("\n","")
+	answers_a.append(answer_ordered)
+ 
+answers_bstyle = []
+for answer in answers:
+	answers_b.append(answer.split("\n"))
+len_answers = len(answers_b)
+
+
+def part_b():
+	final_list= []
+	for i in range(len(answers_a)):
+		results = []
+		count = 0
+		for char in abc:
+			occ = answers_a[i].count(char)
+			if occ == len(answers_b[i]):
+				count += 1
+				results.append(1)
+		each_sum = sum(results)
+		final_list.append(each_sum)
+	print("final sum: ",sum(final_list))
+			
+	
+
+part_b()
+
+
+       
+
