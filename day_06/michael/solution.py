5b1c8ad24711cd2325b6d19a27fb6b90bfa295fe
MW <michael.willig@gs1.nl>
2020-12-06 07:49:48 +0100

    day 6

--- day_06/michael/solution.py
+++ day_06/michael/solution.py
@@ -23,7 +23,14 @@ def read_input_text():
 
 
 def part_a():
-    pass
+    grouptexts = read_input_text().split("\n\n")
+    groupconcats = [x.replace("\n","") for x in grouptexts]
+    print(sum([len(set(x)) for x in groupconcats]))
 
 def part_b():
-    pass
+    grouptexts = read_input_text().split("\n\n")
+    linesAsSets = [[set(y) for y in x.split()] for x in grouptexts]
+    print(sum([len(reduce(operator.__and__, x )) for x in linesAsSets]))
+
+part_a()
+part_b()
