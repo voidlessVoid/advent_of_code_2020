48c80a6d8a7e56d8fc86859192b5c444ddc72969
hacki <sebastian.hackler@gmx.de>
2020-12-08 15:43:03 +0100

    WIP

--- day_08/hacki/day_08.py
+++ day_08/hacki/day_08.py
@@ -12,85 +12,101 @@ def script_run(acc, ind, cmds):
         ind += 1
     if cmds[ind][0] == 'jmp':
         ind += cmds[ind][1]
-    print(cmds[ind])
+
     return {
-        'accumulator':acc,
-        'current_index':ind,
-        'cmds':cmds
+        'accumulator': acc,
+        'current_index': ind,
     }
 
-def part_a(command_ls, input_ls):
+
+def part_a(f_name):
+
+    in_ls = misc.load_input_to_list(f_name)
+
+    in_ls = [
+        i.split(r' ') for i in in_ls
+    ]
+    command_list = [
+        i[0] for i in in_ls
+    ]
+    command_input = [
+        int(re.sub(r'[+]', '', i[1])) for i in in_ls
+    ]
 
     commands = [
         [i, j] for i, j in zip(command_list, command_input)
     ]
-
     accumulator = 0
     execute_index = 0
-    execute_index_new =[]
+    execute_index_new = []
 
     while execute_index not in execute_index_new:
-
         execute_index_new.append(execute_index)
-        script_ = script_run(acc=accumulator,ind=execute_index,cmds=commands)
+        script_ = script_run(
+            acc=accumulator,
+            ind=execute_index,
+            cmds=commands
+        )
         accumulator, execute_index = script_['accumulator'], script_['current_index']
 
     return accumulator
 
-def reverse_ls(ls):
-    reverse_ls = [i for i in reversed(command_list)]
-    return reverse_ls
+# f_name = 'puzzle_input'
 
 f_name = 'test_puzzle'
 
-#f_name = 'puzzle_input'
-
-in_ls = misc.load_input_to_list(f_name)
-
-in_ls = [
-    i.split(r' ') for i in in_ls
-]
-command_list = [
-    i[0] for i in in_ls
-]
-command_input = [
-    int(re.sub(r'[+]','',i[1])) for i in in_ls
-]
-
-def part_b(command_ls, input_ls):
+def part_b(f_name):
     import random
 
     accumulator = 0
     execute_index = 0
-    execute_index_new = [0]
 
-    while max(execute_index_new) < len(command_ls)-1:
-        execute_index_new.append(execute_index)
-        if execute_index > len(command_ls)-1:
+
+
+    while True:
+        in_ls = misc.load_input_to_list(f_name)
+        in_ls = [
+            i.split(r' ') for i in in_ls
+        ]
+        command_list = [
+            i[0] for i in in_ls
+        ]
+        command_input = [
+            int(re.sub(r'[+]', '', i[1])) for i in in_ls
+        ]
+
+        i = random.randint(0, len(command_list) - 1)
+
+        if execute_index > len(command_list) - 1:
             break
-        elif execute_index == len(command_ls)-1:
+        elif execute_index == len(command_list) - 1:
             print('hurra')
             break
-        i = random.randint(0, len(command_ls)-1)
-        if command_ls[i] == 'nop':
-            command_ls[i] = 'jmp'
-        if command_ls[i] == 'jmp':
-            command_ls[i] = 'nop'
+        else:
+            if command_list[i] == 'nop':
+                command_list[i] = 'jmp'
+            if command_list[i] == 'jmp':
+                command_list[i] = 'nop'
+
         commands = [
             [i, j] for i, j in zip(command_list, command_input)
         ]
-        script_ = script_run(acc=accumulator, ind=execute_index, cmds=commands)
-        execute_index = script_['current_index']
 
+        script_ = script_run(
+                acc=accumulator,
+                ind=execute_index,
+                cmds=commands
+            )
+        execute_index = script_['current_index']
 
     return
 
-a = part_b(
-    command_ls = command_list,
-    input_ls = command_input,
+
+a = part_a(
+    f_name
+)
+b = part_b(
+    f_name
 )
 
-print(a)
-
-
-
+print(b)
