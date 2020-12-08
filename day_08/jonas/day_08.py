d071901667fdb8cc7c4edf12dff8ccf6c81648de
js-py20 <75324584+js-py20@users.noreply.github.com>
2020-12-08 20:07:00 +0100

    Create day_08.py

---
+++ day_08/jonas/day_08.py
@@ -0,0 +1,39 @@
+def read_input_lines():
+    with open('input_08.txt', 'r') as fh:
+        return [x.strip() for x in fh.readlines()]
+
+boot = read_input_lines()
+boot_sep = []
+for x in boot:
+	a = x.split()
+	boot_sep.append(a)
+
+position = 0
+accumulator = 0
+step = 0
+tested_steps = []
+
+
+while position not in tested_steps:
+	x = boot_sep[position]
+	if x[0] == nop:
+		tested_steps.append(position)
+		position += 1
+		step +=1
+		
+	if x[0] == acc :
+		accumulator += int(x[1])
+		tested_steps.append(position)
+		position +=1
+		step +=1
+		
+	if x[0] == jmp:
+		tested_steps.append(position)
+		position += int(x[1])
+		step +=1
+		
+print("Accumulator", accumulator)
+#print(tested_steps)
+
+		
+
