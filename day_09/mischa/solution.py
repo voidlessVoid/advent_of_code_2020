2c45d5ca37a03bfef501c5ed785fd69ca7ff1c85
voidlessVoid <58170899+voidlessVoid@users.noreply.github.com>
2020-12-09 07:53:21 +0100

    day9_part1

--- day_09/mischa/solution.py
+++ day_09/mischa/solution.py
@@ -7,7 +7,7 @@ import datetime
 import operator
 from copy import deepcopy
 from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
+from itertools import cycle, combinations
 from functools import reduce
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
@@ -21,9 +21,24 @@ def read_input_text():
     with open('input.txt', 'r') as fh:
         return fh.read().strip()
 
+data = read_input_lines()
+def part_a(inp):
+    p = 25
+    act_num = 25
+    for i in inp[act_num:]:
+        pre_nums = [int(x) for x in inp[act_num-p:act_num]]
+        all_comb = [x for x in combinations(pre_nums,2)]
+        count = 0
+        for x in all_comb:
+            if sum(x) == int(inp[act_num]):
+                count+=1
+                break
+        if count != 1:
+            print(act_num,inp[act_num])
+            return
+        act_num += 1
 
-def part_a():
-    pass
+part_a(data)
 
 def part_b():
     pass
