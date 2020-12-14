ccd64d1463d64bd5661e02553b69e15e96428fc0
Dominik Loroch <36048261+Arktos09@users.noreply.github.com>
2020-12-14 23:55:40 +0100

    Day 14

--- day_14/dominik/main.py
+++ day_14/dominik/main.py
@@ -1,37 +1,42 @@
-#"{0:b}".format(37)
 import re
 from itertools import product
-in_file="input.txt"
+
+in_file = "input.txt"
+
+
 def read_input_lines():
     with open(in_file, 'r') as fh:
-        in_string=fh.read().replace("\n"," ")
+        in_string = fh.read().replace("\n", " ")
     return in_string.split("mask = ")[1:]
 
+
 def format_input():
-    data=read_input_lines()
-    pattern= re.compile("mem\[(\d+)\] = (\d+)")
-    data_list=[]
+    data = read_input_lines()
+    pattern = re.compile("mem\[(\d+)\] = (\d+)")
+    data_list = []
     for line in data:
-        addresses=re.findall(pattern,line)
-        data_list.append([line[:36]]+addresses)
+        addresses = re.findall(pattern, line)
+        data_list.append([line[:36]] + addresses)
     return data_list
+
 
 def intstr_to_bin(value):
     return f"{int(value):b}"
 
+
 def puzzle1():
-    data= format_input()
-    memory={}
+    data = format_input()
+    memory = {}
     for n in data:
-        mask=n[0]
-        mask_ind=[(i,v) for i,v in enumerate(mask)if v !="X"]
+        mask = n[0]
+        mask_ind = [(i, v) for i, v in enumerate(mask) if v != "X"]
         for address in n[1:]:
-            value=intstr_to_bin(address[1])
-            value36=value.rjust(36,"0")
+            value = intstr_to_bin(address[1])
+            value36 = value.rjust(36, "0")
             for m in mask_ind:
-                value36=value36[:m[0]]+m[1]+value36[m[0]+1:]
-            memory[address[0]]=value36
-    result=sum([int(value,2) for value in memory.values()])
+                value36 = value36[:m[0]] + m[1] + value36[m[0] + 1:]
+            memory[address[0]] = value36
+    result = sum([int(value, 2) for value in memory.values()])
     print(f"Sum of memory values: {result}")
 
 
@@ -45,11 +50,11 @@ def puzzle2():
             value = intstr_to_bin(address[0])
             value36 = value.rjust(36, "0")
 
-            x_list=product(["0","1"],repeat=len([x[1] for x in mask_ind if x[1]=="X"]))
-            x_add=[x[0] for x in mask_ind if x[1]=="X"]
-            x_replace=[]
+            x_list = product(["0", "1"], repeat=len([x[1] for x in mask_ind if x[1] == "X"]))
+            x_add = [x[0] for x in mask_ind if x[1] == "X"]
+            x_replace = []
             for x in x_list:
-                x_replace.append(list(zip(x_add,x)))
+                x_replace.append(list(zip(x_add, x)))
 
             for m in mask_ind:
                 value36 = value36[:m[0]] + m[1] + value36[m[0] + 1:]
@@ -59,6 +64,7 @@ def puzzle2():
                 memory_register[value36] = address[1]
     result = sum([int(value) for value in memory_register.values()])
     print(f"Sum of memory values: {result}")
+
 
 if __name__ == '__main__':
     puzzle1()
