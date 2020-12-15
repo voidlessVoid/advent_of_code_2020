e6b14bc14508ef87a0ac18eb590d463c3d490baf
MW <michael.willig@gs1.nl>
2020-12-15 07:38:33 +0100

    day 15

--- day_15/michael/solution.py
+++ day_15/michael/solution.py
@@ -13,17 +13,38 @@ from functools import reduce
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
 
+
 def read_input_lines():
     with open('input.txt', 'r') as fh:
         return [x.strip() for x in fh.readlines()]
+
 
 def read_input_text():
     with open('input.txt', 'r') as fh:
         return fh.read().strip()
 
 
+def solve(upto):
+    start = [18, 11, 9, 0, 5, 1]
+    lastSpoken = dict(zip(start[:-1], range(1, len(start))))
+    last = start[-1]
+    for tstep in range(len(start) + 1, upto + 1):
+        if last in lastSpoken:
+            nextNumber = (tstep - lastSpoken[last]) - 1
+        else:
+            nextNumber = 0
+        lastSpoken[last] = tstep - 1
+        last = nextNumber
+    print(nextNumber)
+
+
 def part_a():
-    pass
+    solve(2020)
+
 
 def part_b():
-    pass
+    solve(30000000)
+
+
+part_a()
+part_b()
