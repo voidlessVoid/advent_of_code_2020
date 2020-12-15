384002e1fed349a4a1c42d849d5f743409d7771d
Dominik Loroch <36048261+Arktos09@users.noreply.github.com>
2020-12-15 07:53:46 +0100

    Day 15

--- day_15/dominik/main.py
+++ day_15/dominik/main.py
@@ -1,30 +1,33 @@
-data=[2,0,6,12,1,3]
+data = [2, 0, 6, 12, 1, 3]
 
-def puzzle1(data,value=2020):
-    turn_dict={}
-    for i,x in enumerate(data):
-        turn_dict[x]=(i+1,i+1)
-    last_turn_nr=data[-1]
-    turn=len(data)+1
+
+def puzzle1(data, value=2020):
+    turn_dict = {}
+    for i, x in enumerate(data):
+        turn_dict[x] = (i + 1, i + 1)
+    last_turn_nr = data[-1]
+    turn = len(data) + 1
     while not turn > value:
-        p,l=turn_dict[last_turn_nr]
-        if p-l==0:
-            turn_nr=0
+        p, l = turn_dict[last_turn_nr]
+        if p - l == 0:
+            turn_nr = 0
         else:
-            turn_nr=l-p
+            turn_nr = l - p
         if turn_nr in turn_dict.keys():
-            turn_dict[turn_nr]=(turn_dict[turn_nr][1],turn)
+            turn_dict[turn_nr] = (turn_dict[turn_nr][1], turn)
         else:
             turn_dict[turn_nr] = (turn, turn)
-        last_turn_nr=turn_nr
-        print(f"Turn {turn}",end="\r")
+        last_turn_nr = turn_nr
+        print(f"Turn {turn}", end="\r")
         turn += 1
 
     print(f"Turn {turn}: Number= {last_turn_nr}")
 
-def puzzle2(data,value):
+
+def puzzle2(data, value):
     puzzle1(data, value)
+
 
 if __name__ == '__main__':
     puzzle1(data)
-    puzzle2(data,value=30000000)
+    puzzle2(data, value=30000000)
