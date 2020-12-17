96a1c3fb2f535150a368001c5320ac2ea8c84d35
MW <michael.willig@gs1.nl>
2020-12-17 07:23:26 +0100

    day 17

--- day_17/michael/solution.py
+++ day_17/michael/solution.py
@@ -1,14 +1,6 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
 from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
+from collections import defaultdict
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
@@ -23,7 +15,58 @@ def read_input_text():
 
 
 def part_a():
-    pass
+    def getAjacent(loc):
+        x,y,z = loc
+        deltas = [-1,0,1]
+        steps = {(dx,dy,dz) for dx in deltas for dy in deltas for dz in deltas} - {(0,0,0)}
+        return {(x+dx,y+dy,z+dz) for dx,dy,dz in steps}
+
+    lines =  read_input_lines()
+    active = {(x,y,0) for y in range(len(lines)) for x in range(len(lines[0])) if lines[y][x] == "#"}
+
+    for _ in range(6):
+        counts = defaultdict(int)
+        for loc in active:
+            for ajacent in getAjacent(loc):
+                counts[ajacent] +=1
+
+        nextCycleActive = set()
+        for ajacent, c in counts.items():
+            if c == 3:
+                nextCycleActive.add(ajacent)
+            elif (c == 2) and (ajacent in active):
+                nextCycleActive.add(ajacent)
+
+        active = nextCycleActive
+
+    print(len(active))
 
 def part_b():
-    pass
+    def getAjacent(loc):
+        x, y, z, a = loc
+        deltas = [-1, 0, 1]
+        steps = {(dx, dy, dz, da) for dx in deltas for dy in deltas for dz in deltas for da in deltas} - {(0, 0, 0, 0)}
+        return {(x + dx, y + dy, z + dz, a + da) for dx, dy, dz, da in steps}
+
+    lines = read_input_lines()
+    active = {(x, y, 0, 0) for y in range(len(lines)) for x in range(len(lines[0])) if lines[y][x] == "#"}
+
+    for _ in range(6):
+        counts = defaultdict(int)
+        for loc in active:
+            for ajacent in getAjacent(loc):
+                counts[ajacent] += 1
+
+        nextCycleActive = set()
+        for ajacent, c in counts.items():
+            if c == 3:
+                nextCycleActive.add(ajacent)
+            elif (c == 2) and (ajacent in active):
+                nextCycleActive.add(ajacent)
+
+        active = nextCycleActive
+
+    print(len(active))
+
+part_a()
+part_b()
