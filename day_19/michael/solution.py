e4edbf5da70138fe845d1766477bba8140ed3617
MW <michael.willig@gs1.nl>
2020-12-18 08:07:42 +0100

    day 19

--- day_19/michael/solution.py
+++ day_19/michael/solution.py
@@ -1,14 +1,5 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
+from collections import deque
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
@@ -23,7 +14,73 @@ def read_input_text():
 
 
 def part_a():
-    pass
+    def evalstring(instr,ix):
+
+        running = None
+        operator = None
+        val = None
+        while ix < len(instr):
+            if instr[ix].isdigit():
+                val = int(instr[ix])
+            elif instr[ix] == "(":
+                val, ix = evalstring(instr,ix+1)
+            elif instr[ix] in "+*":
+                operator = instr[ix]
+
+            elif instr[ix] == ")":
+                return [running, ix]
+
+            if val:
+                if running is None:
+                    running= int(val)
+                else:
+                    running  = eval(f"{running} {operator} {val}")
+                    operator = None
+
+            val = None
+            ix+=1
+        return running
+
+    print(sum([evalstring(x,0) for x in read_input_lines()]))
+
 
 def part_b():
-    pass
+    def evalstring(instr, ix):
+
+        parseTokens = []
+
+        while ix < len(instr):
+            if instr[ix].isdigit():
+                parseTokens.append(int(instr[ix]))
+            elif instr[ix] == "(":
+                val, ix = evalstring(instr, ix + 1) #we evaluate the bracket section and val goes on the tokenlist
+                parseTokens.append(val)
+            elif instr[ix] in "+*":
+                parseTokens.append(instr[ix])
+
+            elif instr[ix] == ")":
+                break
+            ix += 1
+
+        # first handle all the + operations. Afterwards we can just eval the rest in a straightforward way
+        parseRight = deque(parseTokens)
+        parseLeft = deque([])
+
+        while parseRight:
+            token = parseRight.popleft()
+            if token == "+":
+                v1 = parseLeft.pop()
+                v2 = parseRight.popleft()
+                token = v1 + v2
+
+            parseLeft.append(token)
+
+        result = eval("".join([str(x) for x in parseLeft]))
+        return result, ix
+
+    print(sum([evalstring(x,0)[0] for x in read_input_lines()]))
+
+part_b()
+
+
+
