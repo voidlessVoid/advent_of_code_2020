a518804e2af983428918d8d915705601659abdd2
MW <michael.willig@gs1.nl>
2020-12-22 14:52:21 +0100

    day 22

--- day_22/michael/solution.py
+++ day_22/michael/solution.py
@@ -1,14 +1,6 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
 from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
-from functools import reduce
+
 
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
@@ -23,7 +15,59 @@ def read_input_text():
 
 
 def part_a():
-    pass
+    p1, p2 = [deque([ int(x) for x in pinput.split("\n")[1:]]) for pinput in read_input_text().split("\n\n")]
+
+    while p1 and p2:
+        c1, c2 = p1.popleft(), p2.popleft()
+        if c1 > c2:
+            p1.append(c1)
+            p1.append(c2)
+        else:
+            p2.append(c2)
+            p2.append(c1)
+
+    finalList = list(p1 or p2)
+    print(sum([finalList[-ix] * ix for ix in range(1, len(finalList) +1)]))
 
 def part_b():
-    pass
+
+    def play(players):
+        states = set()
+
+        while all(players):
+            state = (tuple(players[0]), tuple(players[1]))
+            if state in states:
+                return 0
+            states.add(state)
+
+            c1, c2 = [p.popleft() for p in players]
+            if c1 <= len(players[0]) and c2 <= len(players[1]):
+                winner = play([deque(list(players[0])[:c1]),deque(list(players[1])[:c2])])
+            elif c1 > c2:
+                winner = 0
+            else:
+                winner = 1
+
+            if winner:
+                players[1].append(c2)
+                players[1].append(c1)
+            else:
+                players[0].append(c1)
+                players[0].append(c2)
+
+        if players[0]:
+            return 0
+        else:
+            return 1
+
+
+    players = [deque([ int(x) for x in pinput.split("\n")[1:]]) for pinput in read_input_text().split("\n\n")]
+
+
+    play(players)
+    finalList = list(players[0] or players[1])
+    print(players)
+    print(sum([finalList[-ix] * ix for ix in range(1, len(finalList) +1)]))
+
+part_b()
+# 5870 too low
