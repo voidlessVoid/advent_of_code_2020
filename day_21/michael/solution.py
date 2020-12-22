29fa15e57f835235504c6cf48da41b50e1a37c82
MW <michael.willig@gs1.nl>
2020-12-22 13:29:43 +0100

    day 21

--- day_21/michael/solution.py
+++ day_21/michael/solution.py
@@ -1,15 +1,6 @@
 import os
-import sys
-import pandas as pd
-import numpy as np
-import math
-import datetime
-import operator
-from copy import deepcopy
-from collections import Counter, ChainMap, defaultdict, deque
-from itertools import cycle
 from functools import reduce
-
+import operator
 CURRENT_DIRECTORY = os.path.dirname(__file__)
 os.chdir(CURRENT_DIRECTORY)
 
@@ -23,7 +14,40 @@ def read_input_text():
 
 
 def part_a():
-    pass
+    ingredientStatements = read_input_lines()
+    allIngredientset = set()
+    allIngredientlist = []
+    allergenToPotentialIngredients = {}
+
+    for ingredientStatement in ingredientStatements:
+        ingredients, allergens = ingredientStatement.split("(")
+        ingredients = ingredients.split()
+        allergens = set(allergens[:-1].replace(",", " ").split()) - {"contains"}
+        allIngredientset |= set(ingredients)
+        allIngredientlist += ingredients
+        for allergen in allergens:
+            if allergen in allergenToPotentialIngredients:
+                allergenToPotentialIngredients[allergen] &= set(ingredients)
+            else:
+                allergenToPotentialIngredients[allergen] = set(ingredients)
+
+    safeIngredients = allIngredientset - reduce(operator.__or__, allergenToPotentialIngredients.values())
+    print(len([x for x in allIngredientlist if x in safeIngredients]))
+    return allergenToPotentialIngredients
+
 
 def part_b():
-    pass
+    allergenToPotentialIngredients = part_a()
+    nSolve = len(allergenToPotentialIngredients)
+    solved = {}
+
+    while len(solved) != nSolve:
+        for k,v in list(allergenToPotentialIngredients.items()):
+            remaining_options = v - set(solved.keys())
+            if  len(remaining_options) ==1:
+                solved[remaining_options.pop()] = k
+                del allergenToPotentialIngredients[k]
+
+    print(",".join(sorted(solved.keys(), key = lambda x: solved[x])))
+
+part_b()
